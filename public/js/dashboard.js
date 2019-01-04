89a7a62e7072203c05054e3f2a0e7c253e55fbdd
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-04 15:45:47 -0800

    modified

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,5 @@
 
-
-fetch("/api/test")
+fetch("http://localhost:8080/api/test")
 .then(response => response.json())
 .then(results => {
     console.log(results.data)
@@ -9,3 +8,62 @@ fetch("/api/test")
 .catch(err => {
   $('.js-error-message').html("Whoops! We currently don't have anything available for your search. Please try another search.");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fetches trip data
+function fetchTrips() {
+  fetch("/api/test")
+  .then(response => response.json())
+  .then(results => {
+    console.log(results.data)
+    $(".js-trips-container").html(results.data)
+  })
+  .catch(err => {
+    $('.js-error-message').html("Whoops! Something went wrong.");
+  });
+}
+
+function openModal() {
+
+}
+
+function closeModal() {
+
+}
+
+function addEntry() {
+
+}
+
+function editEntry() {
+
+}
+
+function deleteEntry() {
+
+}
+
+function displaySelectedModal() {  
+
+}
+

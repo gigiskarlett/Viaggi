46cdfbe1ebadfe3c414e9500ac28212cdf9c9713
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-04 16:02:59 -0800

    added function stubs

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,68 +1,43 @@
 
-fetch("http://localhost:8080/api/test")
-.then(response => response.json())
-.then(results => {
-    console.log(results.data)
-    $(".js-trips-container").html(results.data)
-})
-.catch(err => {
-  $('.js-error-message').html("Whoops! We currently don't have anything available for your search. Please try another search.");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //fetches trip data
-function fetchTrips() {
-  fetch("/api/test")
+(function fetchTrips (){
+  fetch("http://localhost:8080/api/test")
   .then(response => response.json())
   .then(results => {
-    console.log(results.data)
-    $(".js-trips-container").html(results.data)
+      console.log(results.data)
+      $(".js-trips-container").html(results.data)
   })
   .catch(err => {
-    $('.js-error-message').html("Whoops! Something went wrong.");
+    $('.js-error-message').html("Whoops! We currently don't have anything available for your search. Please try another search.");
   });
-}
+})();
 
+//opens modal to add trip
 function openModal() {
 
 }
 
+//closes modal
 function closeModal() {
 
 }
 
+//adds trip
 function addEntry() {
 
 }
 
+//edits trip
 function editEntry() {
 
 }
 
+//deletes trip entry
 function deleteEntry() {
 
 }
 
+//show trip modal on click
 function displaySelectedModal() {  
 
 }

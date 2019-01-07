0a87305d5d12eb03babb05c30bdbe19966adf249
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-07 12:45:32 -0800

    trip post schema created

---
+++ models.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const tripPostSchema = mongoose.Schema({
+    destination: {type: String, required: true},
+    when: {type: Date, required: true },
+    lastDayOfTrip: {type: Date, required: true},
+    tripDetails: {type: String}
+});
+
+tripPostSchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        destination: this.destination,
+        when: this.when,
+        lastDayOfTrip: this.lastDayOfTrip,
+        tripDetails: this.tripDetails
+    };
+};
+
+const TripPost = mongoose.model('TripPost', tripPostSchema)
+
+module.exports = {TripPost};

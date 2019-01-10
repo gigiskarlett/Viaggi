71a971e412c6f9e39540bad47953771a0615e63e
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-10 11:11:52 -0800

    changed type date on schema to string

--- models.js
+++ models.js
@@ -1,13 +1,15 @@
 'use strict';
 
+//var dateFormat = require('dateformat');
+
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const tripPostSchema = mongoose.Schema({
     destination: {type: String, required: true},
-    when: {type: Date, required: true },
-    lastDayOfTrip: {type: Date, required: true},
-    tripDetails: {type: String}
+    when: {type: String, required: true },
+    lastDayOfTrip: {type: String, required: true},
+    tripDetails: {type: String, required: true}
 });
 
 tripPostSchema.methods.serialize = function() {

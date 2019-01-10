bd8a0e76624c8d756d4788f6bd4e27a92ed99fad
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-09 17:59:24 -0800

    fetch function for trips

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,33 +1,44 @@
+//binds event handlers
 
-//fetches trip data
-(function fetchTrips (){
-  fetch("http://localhost:8080/api/test")
-  .then(response => response.json())
-  .then(results => {
-      console.log(results.data)
-      $(".js-trips-container").html(results.data)
-  })
-  .catch(err => {
-    $('.js-error-message').html("Whoops! We currently don't have anything available for your search. Please try another search.");
-  });
-})();
+$(() => {
+  fetchTrips();  
+  openEntryModal();
+  closeModal();
+});
 
-//opens modal to add entry
-(function openEntryModal() {
+//// Modal functionality /////
+
+//opens modal when button is clicked
+function openEntryModal() {
   $('#button-container').on('click', '.js-create-button', function() {
   $('.js-modal-container').show();
   });
-})();
+}
 
 
 //closes modal when user clicks cancel
-(function closeModal() {
+function closeModal() {
   $('.js-entry-modal').on('click', '.js-cancel-button', function() {
     confirm("Are you sure you want to close without submitting your trip?");
     $('.js-modal-container').hide();
   })
-})();
+}
 
+//// CRUD operations for trips data /////
+
+//fetches trip data
+function fetchTrips(callback) {
+  fetch("http://localhost:8080/trips")
+  .then(response => response.json())
+  .then(responseJson => {
+      console.log(responseJson)
+      return responseJson
+  })
+  .then(responseJson => callback(responseJson))
+  .catch(err => {
+    $('.js-error-message').html("Whoops! We currently don't have anything available for your search. Please try another search.");
+  });
+}
 const postTrip = (newTripEntry) => {
   fetch('/trips',
   {
@@ -59,6 +70,13 @@ function deleteEntry() {
 
 //show trip modal on click
 function displaySelectedModal() {  
+
+}
+
+//////// manipulation of data ////
+
+function createTripModal(responseJson) {
+
 
 }
 

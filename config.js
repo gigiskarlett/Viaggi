b1086f2dbd6baaee5d7853d1df69f038f6202576
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-15 03:36:46 -0800

    modified

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/viaggi';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-viaggi';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/viaggi';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/test-viaggi';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';

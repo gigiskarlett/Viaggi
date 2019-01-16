8bf521c96cfc6b61aad82ab630d98c21a8a4c613
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-01-15 23:52:27 -0800

    password validation

--- public/js/register.js
+++ public/js/register.js
@@ -1,22 +1,25 @@
-//init function
-$(() => {
-    newUserEntry() 
-});
+//verifies password match 
+$('#js-password, #js-confirm-password').on('keyup', function(event) {
+    if ($('#js-password').val() == $('#js-confirm-password').val()) {
+        $('#js-error-message').html('Passwords Match');
+    } else {
+        $('#js-error-message').html(`Password Doesn't Match`);
+    }       
+});  
 
 //listens for submit of new user 
-function newUserEntry() {
-    $('.js-submit-form').on('click', '.js-signup-button', function(event) {
-        event.preventDefault();
-        let newUser = {};
-        newUser.username = $('.js-username').val();
-        newUser.fullName = $('.js-full-name').val();
-        newUser.password = $('.js-password').val();
-        submitUser(newUser)
-        $('.js-username').val('');
-        $('.js-full-name').val('')
-        $('.js-password').val('');
-    });
-};
+$('.js-submit-form').on('click', '.js-signup-button', function(event) {
+    event.preventDefault();
+    let newUser = {};
+    newUser.username = $('.js-username').val();
+    newUser.fullName = $('.js-full-name').val();
+    newUser.password = $('.js-password').val();
+    submitUser(newUser)
+    $('.js-username').val('');
+    $('.js-full-name').val('')
+    $('.js-password').val('');
+});
+
 
 function submitUser(newUser) {
     fetch('/api/users', 
